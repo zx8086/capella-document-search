77da87a02764808355b87304a2522edb32d3418e
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-09-09 06:52:32 +0200

    Add smaller file videos

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -214,7 +214,13 @@
     const videos = [
         "/idle-videos/X1_Single_Lewis_Hamilton-GENERIC_1280x730.mp4",
         "/idle-videos/FA24_TH_T1_OCTOBER_DUO_10_B_ PAID_ LOGO_SOUND_1920_1080.mp4",
+        "/idle-videos/ECOM_TOMMY_STRAY_KIDS_6sec_001_3412x1892_MP4_Audio_NoLogo.mp4",
         "/idle-videos/FA24_TH_T1_SEPTEMBER_ABBEY_6_C_ECOM_ NO LOGO_SOUND_3412_1892.mp4",
+        "/idle-videos/X1_DUO_GR_LH-GENERIC_1280x730.mp4",
+        "/idle-videos/ECOM_TOMMY_STRAY_KIDS_6sec_002_3412x1892_MP4_Audio_NoLogo.mp4",
+        "/idle-videos/FA24_TH_T1_OCTOBER_DUO_6_A_ECOM_ NO LOGO_SOUND_3412_1892.mp4",
+        "/idle-videos/ECOM_TOMMY_STRAY_KIDS_6sec_003_3412x1892_MP4_Audio_NoLogo.mp4",
+        "/idle-videos/FA24_TH_T1_SEPTEMBER_PATRIC_6_B_ECOM_ NO LOGO_SOUND_3412_1892.mp4",
     ];
 </script>
 
