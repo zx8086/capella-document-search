7c81590ff50299ae0ac008434e118f1939d6aca0
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-09-13 01:05:23 +0200

    Added all endpoints

--- static/runtime-config.js
+++ static/runtime-config.js
@@ -2,9 +2,10 @@
 
 window.ENV = {
   VITE_ELASTIC_APM_SERVICE_NAME: "Capella Document Search",
-  VITE_ELASTIC_APM_SERVER_URL: "https://apm.siobytes.com",
+  VITE_ELASTIC_APM_SERVER_URL:
+    "https://eu-b2b.apm.eu-central-1.aws.cloud.es.io",
   VITE_ELASTIC_APM_SERVICE_VERSION: "2.0.0",
-  VITE_ELASTIC_APM_ENVIRONMENT: "development",
+  VITE_ELASTIC_APM_ENVIRONMENT: "production",
   VITE_ELASTIC_APM_DISTRIBUTED_TRACING_ORIGINS: "['*']",
 };
 //
